06982fc342ec0176bf9940cd375bae07e6d34ac8
Vlad Nesterov <53149984+Vlanesvit@users.noreply.github.com>
2024-06-27 21:49:17 +0800

    bugfix

--- js/app.js
+++ js/app.js
@@ -1,41 +1,3 @@
-function preloader() {
-	const preloader = document.querySelector('.preloader');
-
-	const imgs = preloader.querySelectorAll('.preloader__imgs img')
-	const progress = preloader.querySelectorAll('.preloader__line-progress')
-
-	// Скорость
-	let durationImg = 150;
-	let durationLine = 1000;
-
-	//========================================================================================================================================================
-
-
-	let now = -1;
-	function changeImage() {
-		// Убираем все картинки
-		for (let i = 0; i < imgs.length; i++) {
-			imgs[i].style.display = 'none';
-		}
-		// Показываем поочередно
-		if ((now + 1) == imgs.length) {
-			now = 0;
-		} else {
-			now++;
-		}
-		imgs[now].style.display = 'block';
-	}
-	// Повторяем каждые duration
-	setInterval(function () {
-		changeImage()
-	}, durationImg);
-
-	//========================================================================================================================================================
-
-	progress
-}
-
-
 (function () {
 	'use strict';
 	const dBody = document.body;
@@ -46,25 +8,27 @@ function preloader() {
 			preloaderImgs = preloader.querySelectorAll('.preloader__imgs img'),
 			progressBar = preloader.querySelector('.preloader__line-progress'),
 			url = document.location.href,
-			images = document.querySelectorAll('img'),
+			images = document.querySelectorAll('img:not(.preloader__imgs img)'),
 			max = 100,
 			xhr = new XMLHttpRequest();
 
+			console.log(images);
+
 		let loadingProgress = 0,
 			start = 0,
-			duration = 150;
+			duration = 80;
 
 		function animateBar(progress) {
 			let interval = setInterval(() => {
 				if (start < progress && start < max) {
 					start++;
 
-					// if (start == max) {
-					// 	clearInterval(interval);
-					// 	setTimeout(function () {
-					// 		preloader.style.cssText = "opacity: 0; visibility: hidden;";
-					// 	}, 1);
-					// }
+					if (start == max) {
+						clearInterval(interval);
+						setTimeout(function () {
+							preloader.style.cssText = "opacity: 0; visibility: hidden;";
+						}, 1);
+					}
 
 					let tr = -100 + start;
 
