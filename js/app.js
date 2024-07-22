09a930e2a697668725b028c5df576e1a71f0ae7e
Vlad Nesterov <53149984+Vlanesvit@users.noreply.github.com>
2024-07-22 19:01:26 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,49 +8,53 @@
 		finishImg = document.querySelector('.preloader__imgs img[src*="finish"]'),
 		cacheCleared = sessionStorage.getItem('cacheCleared') === null;
 
-	let start = 0,
-		preloadDuration = 10000,
-		nowImg = -1,
-		ImgChangeDuration = 60;
+	let nowImg = -1,
+		preloadDuration = 10000, // 10 секунд
+		ImgChangeDuration = 60; // Изменение изображения каждые 1 секунда
 
-	function showNextImage() {
-		// Функция для изменения изображения в прелоадере
-		function changeImage() {
-			preloaderImgs.forEach(img => img.style.display = 'none');
-			nowImg = (nowImg + 1) % preloaderImgs.length;
-			preloaderImgs[nowImg].style.display = 'block';
-		}
-
-		setInterval(changeImage, ImgChangeDuration);
+	// Функция для изменения изображения в прелоадере
+	function changeImage() {
+		preloaderImgs.forEach(img => img.style.display = 'none');
+		nowImg = (nowImg + 1) % preloaderImgs.length;
+		preloaderImgs[nowImg].style.display = 'block';
 	}
 
-	function hidePreload() {
+	// Функция для запуска анимации прогресс-бара
+	function startProgressBar() {
+		preloadProgressLine.style.transition = `width ${preloadDuration}ms cubic-bezier(0.11, 0, 0.5, 0)`;
+		preloadProgressLine.style.width = '100%';
+	}
+
+	// Функция для скрытия прелоадера
+	function hidePreloader() {
 		preloader.style.opacity = '0';
 		preloader.style.visibility = 'hidden';
 	}
 
-	function completePreloading() {
-		preloadProgressLine.style.width = '100%';
-		preloadProgressLine.style.transitionDuration = (preloadDuration - 2000) + 'ms';
-		preloadProgressLine.addEventListener('transitionend', function () {
+	// Основная функция для анимации и показа прелоадера
+	function startPreloader() {
+		startProgressBar();
+		setInterval(changeImage, ImgChangeDuration);
+		setTimeout(() => {
 			preloadProgress.classList.add('_ended')
-		});
+			setTimeout(() => {
+				hidePreloader();
+				sessionStorage.setItem('cacheCleared', 'false');
+			}, 2000); // Дополнительные 2 секунды для завершения всех анимаций
+		}, preloadDuration);
 	}
 
 	if (cacheCleared) {
 		// При первом заходе или очищенном кеше показываем все изображения прелоадера
-		showNextImage();
-		completePreloading();
-		setTimeout(() => {
-			hidePreload();
-			sessionStorage.setItem('cacheCleared', 'false');
-		}, preloadDuration);
+		startPreloader();
 	} else {
 		// При обновлении страницы показываем только финальное лого и привязываем его к загрузке DOM
 		preloaderImgs.forEach(img => img.style.display = 'none');
 		finishImg.style.display = 'block';
-		setTimeout(() => {
-			hidePreload()
-		}, 1000);
+		window.addEventListener('DOMContentLoaded', () => {
+			setTimeout(() => {
+				hidePreloader();
+			}, 1000);
+		});
 	}
 })();
